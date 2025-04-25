8b8ce2101c0a72e9e1175fa2a535f8e46df0bdad
Subramanian T <subramanian638545@gmail.com>
2025-04-25 22:25:01 +0530

    Fix: Add Render URL to ALLOWED_HOSTS

--- youtube_harvest/settings.py
+++ youtube_harvest/settings.py
@@ -137,3 +137,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+ALLOWED_HOSTS = ['*']
